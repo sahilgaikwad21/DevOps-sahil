5b5b631d2b8084521505a5707bbfd148058a2981
root <root@ip-172-31-46-23.eu-north-1.compute.internal>
2023-06-24 14:12:10 +0000

    Added server code

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -25,48 +25,48 @@ SECRET_KEY = '8)810zj@#^2xp=1=2rkozbv8#)gub6m1a^9qf&)d-9&x9*c2a_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'todos.apps.TodosConfig',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
+            'todos.apps.TodosConfig',
+                'django.contrib.admin',
+                    'django.contrib.auth',
+                        'django.contrib.contenttypes',
+                            'django.contrib.sessions',
+                                'django.contrib.messages',
+                                    'django.contrib.staticfiles',
+                                    ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+            'django.middleware.security.SecurityMiddleware',
+                'django.contrib.sessions.middleware.SessionMiddleware',
+                    'django.middleware.common.CommonMiddleware',
+                        'django.middleware.csrf.CsrfViewMiddleware',
+                            'django.contrib.auth.middleware.AuthenticationMiddleware',
+                                'django.contrib.messages.middleware.MessageMiddleware',
+                                    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+                                    ]
 
 ROOT_URLCONF = 'todoApp.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+            {
+                        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                                'DIRS': [],
+                                        'APP_DIRS': True,
+                                                'OPTIONS': {
+                                                                'context_processors': [
+                                                                                    'django.template.context_processors.debug',
+                                                                                                    'django.template.context_processors.request',
+                                                                                                                    'django.contrib.auth.context_processors.auth',
+                                                                                                                                    'django.contrib.messages.context_processors.messages',
+                                                                                                                                                ],
+                                                                        },
+                                                    },
+            ]
 
 WSGI_APPLICATION = 'todoApp.wsgi.application'
 
@@ -75,30 +75,30 @@ WSGI_APPLICATION = 'todoApp.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+            'default': {
+                        'ENGINE': 'django.db.backends.sqlite3',
+                                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+                                    }
+            }
 
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+            {
+                        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+                            },
+                {
+                            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+                                },
+                    {
+                                'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+                                    },
+                        {
+                                    'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+                                        },
+                        ]
 
 
 # Internationalization
@@ -125,5 +125,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
+            os.path.join(BASE_DIR, 'staticfiles'),
+            )
+
